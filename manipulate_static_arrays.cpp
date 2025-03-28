e1319d1bbc05a4969501c1296b0698f78a1b267c
WizzOFF <maher_off@proton.me>
2025-03-29 00:55:56 +0100

    manipulate_static_arrays.cpp: declare a static array

---
+++ manipulate_static_arrays.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int meilleurScore[5];       //Déclare un tableau de 5 int 
+
+   double anglesTriangle[3];   //Déclare un tableau de 3 double
+
+   return 0;
+}
