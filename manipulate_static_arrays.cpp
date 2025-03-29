8d36725a32ac5d3b608f4edc2d14e35bda62635d
WizzOFF <maher_off@proton.me>
2025-03-29 01:08:22 +0100

    manipulate_static_arrays.cpp: access elements of a static array

--- manipulate_static_arrays.cpp
+++ manipulate_static_arrays.cpp
@@ -8,5 +8,11 @@ int main()
    
    int meilleursScores[nombreMeilleursScores];   //Déclaration du tableau
 
+   meilleursScores[0] = 118218;  //Remplissage de la première case
+   meilleursScores[1] = 100432;  //Remplissage de la deuxième case
+   meilleursScores[2] = 87347;   //Remplissage de la troisième case
+   meilleursScores[3] = 64523;   //Remplissage de la quatrième case
+   meilleursScores[4] = 31415;   //Remplissage de la cinquième case
+   
    return 0;
 }
