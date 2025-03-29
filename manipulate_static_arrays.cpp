efe98731ce0d238f4c5307444f0f4d04dfbbdeda
WizzOFF <maher_off@proton.me>
2025-03-29 02:31:31 +0100

    manipulate_static_arrays.cpp: use case > static arrays with functions

--- manipulate_static_arrays.cpp
+++ manipulate_static_arrays.cpp
@@ -23,6 +23,23 @@ int main()
 }
 */
 
+/*
+ *  Fonction qui calcule la moyenne des éléments d'un tableau
+ *  - tableau : Le tableau dont on veut la moyenne
+ *  - tailleTableau : La taille du tableau
+ */
+ double moyenne(double tableau[], int tailleTableau)
+ {
+    double moyenne(0);
+    for(int i(0); i<tailleTableau; ++i)
+    {
+       moyenne += tableau[i];   //On additionne toutes les valeurs
+    }
+    moyenne /= tailleTableau;
+ 
+    return moyenne;
+}
+
 int main()
 {
     int const nombreNotes(6);
@@ -35,17 +52,10 @@ int main()
     notes[4] = 14.5;
     notes[5] = 15;
 
-    double moyenne(0);
-    for(int i(0); i<nombreNotes; ++i)
-    {
-        moyenne += notes[i];   //On additionne toutes les notes
-    }
-    //En arrivant ici, la variable moyenne contient la somme des notes (79.5)
-    //Il ne reste donc qu'à diviser par le nombre de notes
+    double moyenneNotes;
+    moyenneNotes = moyenne(notes[], nombreNotes);
 
-    moyenne /= nombreNotes;
-
-    cout << "Votre moyenne est : " << moyenne << endl;
+    cout << "Votre moyenne est : " << moyenneNotes << endl;
 
     return 0;
 }
